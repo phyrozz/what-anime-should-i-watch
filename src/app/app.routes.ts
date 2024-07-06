0eb41f20411da114a7ccb43d2f8068948e0f4e75
phyrozz <joshuamalabanan70@gmail.com>
2024-07-06 23:58:09 +0800

    removed unused routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
 import { AnimeRecommendationsComponent } from './components/anime-recommendations/anime-recommendations.component';
-import { AnimeDetailComponent } from './components/anime-detail/anime-detail.component';
 
 export const routes: Routes = [
-  { path: '', component: AnimeRecommendationsComponent },
-  { path: 'anime/:id', component: AnimeDetailComponent }
+  { path: '', component: AnimeRecommendationsComponent }
 ];
