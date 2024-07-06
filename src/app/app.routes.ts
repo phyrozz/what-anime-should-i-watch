599192b801ba63b66701b55a85f5138bc30bb873
phyrozz <joshuamalabanan70@gmail.com>
2024-07-06 14:31:34 +0800

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { AnimeRecommendationsComponent } from './components/anime-recommendations/anime-recommendations.component';
-import { AnimeDetailComponent } from './components/anime-detail/anime-detail.component';
-
-export const routes: Routes = [
-  { path: '', component: AnimeRecommendationsComponent },
-  { path: 'anime/:id', component: AnimeDetailComponent }
-];
